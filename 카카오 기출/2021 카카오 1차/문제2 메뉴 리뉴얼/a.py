4bb2a61cfa5e36d774cd38ff97c88b6a5f6aa26a
kkad45@naver.com <kkad45@naver.com>
2021-05-20 22:47:57 +0900

    백준 구현강의 1번째 문제 내일다시풀기

--- 카카오 기출/2021 카카오 1차/문제2 메뉴 리뉴얼/a.py
+++ 카카오 기출/2021 카카오 1차/문제2 메뉴 리뉴얼/a.py
@@ -12,8 +12,7 @@ def solution(orders,course):
             combi = combinations(sorted(order),c)
             temp += combi
         counter = Counter(temp)
-        print(counter)
-        print(counter.values())
+        print(temp)
         if len(counter) != 0 and max(counter.values()) != 1:
             answer += [''.join(f) for f  in counter if counter[f] == max(counter.values())]
     return sorted(answer)
