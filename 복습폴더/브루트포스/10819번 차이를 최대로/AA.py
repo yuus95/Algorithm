914fa1bfef0278ad216e4deca54137264659a2a6
yushin <kkad45@naver.com>
2021-07-04 19:47:53 +0900

    0704 차이를 최대로

---
+++ 복습폴더/브루트포스/10819번 차이를 최대로/AA.py
@@ -0,0 +1,50 @@
+import sys
+sys.stdin=open("00.txt","r")
+
+
+
+def next_permutation(a):
+    i= len(a)-1
+    while  i > 0 and a[i-1] >=a[i]:
+         i -=1
+    if i <= 0 :
+        return False
+    j = len(a)-1
+    while a[i-1] >=a[j]:
+        j-=1
+
+    a[i-1], a[j] = a[j],a[i-1]
+
+    j = len(a)-1
+
+    while i <j :
+        a[i],a[j]= a[j],a[i]
+        i+=1
+        j-=1
+    return True
+
+
+
+n = int(input())
+
+a = list(map(int, input().split()))
+a.sort()
+ans = 0
+for i  in range(n-1):
+    ans += abs(a[i]-a[i+1])
+
+while True:
+    if next_permutation(a):
+        temp=0
+        for i in range(n - 1):
+            temp += abs(a[i] - a[i + 1])
+        if temp > ans:
+            ans = temp
+    else:
+        break
+print(ans)
+
+
+
+
+
